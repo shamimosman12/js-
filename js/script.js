4244e7a773b4c96fccb196ecae66d59d901e513c
Shamim Hossain <167690026+shamimosman12@users.noreply.github.com>
2024-08-30 10:58:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//ex1
+function textChange(){
+	document.getElementById('tc').innerHTML="Welcome to shamim's code craft's";
+}
+
+//ex2
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+
+//ex3
+function bulbon(){
+	document.getElementById('bulb').src='images/pic2.gif';
+}
+function bulboff(){
+	document.getElementById('bulb').src='images/pic1.gif';
+}
+
+//ex4
+function changeCss(){
+	document.getElementById('cc').style.color="red";
+}
+
+
+//ex5
+function textShow(){
+	document.getElementById('tsh').style.display="block";
+}
+function textHide(){
+	document.getElementById('tsh').style.display="none";
+}
+
+//ex6
+function happyPhoto(){
+	document.getElementById('think').src='images/happy.jpg';
+}
+function sadPhoto(){
+	document.getElementById('think').src='images/sad(1).jpg';
+}
+
+//ex7
+function changeFont_size(){
+	document.getElementById('fc').style.fontSize="50px";
+}
+//ex8
+function changeBgc(){
+	document.getElementById('fbcg').style.backgroundColor="teal";
+}
+
+//ex9
+function changeBorder(){
+	document.getElementById('cb').style.border= '5px solid black';
+}
+
+//ex10
+function alignItem(){
+	document.getElementById('ai').style.textAlign= 'center';
+}
+
+//ex11
+function cda(){
+	document.getElementById("mDiv").style.animationDelay = "3s";
+}
